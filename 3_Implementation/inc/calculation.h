5b25ee61de5b33d10f507e414381de5e7f534b2c
tridib936 <61946877+tridib936@users.noreply.github.com>
2021-09-08 05:28:46 +0530

    Add files via upload

---
+++ 3_Implementation/inc/calculation.h
@@ -0,0 +1,30 @@
+/**
+ * @file calculation.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-08
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef CALCULATION_H
+#define CALCULATION_H
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+double sum(double array[],int size);
+double difference(double array[],int size);
+double multiply(double array[],int size);
+double divide(double array[],int size);
+double factorial(double array[]);
+double permutation(double array[]);
+double combination(double array[]);
+double logarithm(double array[]);
+double sine(double array[]);
+double cosine(double array[]);
+double tangent(double array[]);
+
+#endif //#ifndef CALCULATION_H
